ad3fa0d310dc7416c03cd6ceaed96c11deecadbb
fran <franklynssierracontreras@gmail.com>
2023-01-21 15:14:40 -0400

    .

--- src/api/post.js
+++ src/api/post.js
@@ -1,25 +1,25 @@
 import axios from 'axios'
-export const getPostsRequest=async()=>await axios.get('/posts')
+export const getPostsRequest=async()=>await axios.get('https://back-mern-api.onrender.com/posts')
 export const createPostRequest=async(post)=>{
    const form=new FormData()
    for(let i in post){
   form.append(i,post[i])
    }
-  return await axios.post('/posts',form,{
+  return await axios.post('https://back-mern-api.onrender.com/posts',form,{
         headers:{
             "Content-Type":"multipart/form-data"
         }
     })
 
 }
-export const DeletePostRequest = async (id) =>await axios.delete("/posts/"+ id);
-export const getPostRequest = async (id) =>await axios.get("/posts/"+ id);
+export const DeletePostRequest = async (id) =>await axios.delete("https://back-mern-api.onrender.com/posts/"+ id);
+export const getPostRequest = async (id) =>await axios.get("https://back-mern-api.onrender.com/posts/"+ id);
 export const updatePostRequest = async (id,newField) =>{
     const form=new FormData()
     for(let i in newField){
    form.append(i,newField[i])
     }
-   return await axios.put(`/posts/${id}`,form,{
+   return await axios.put(`https://back-mern-api.onrender.com/posts/${id}`,form,{
          headers:{
              "Content-Type":"multipart/form-data"
          }
